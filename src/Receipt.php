0aa00c53ad8c40d0d5bca7fe84c0412be23555cf
Gamliel Cohen <gcohen@adventureconseil.com>
2018-06-22 17:54:12 +0200

    style(src): Testing codefactor

--- src/Receipt.php
+++ src/Receipt.php
@@ -7,38 +7,20 @@ namespace TDD;
  */
 class Receipt
 {
-	/**
-	 * @param array $items  Array of items
-	 * @param float $coupon Coupon
-	 * @return int
-	 */
-	public function total(array $items=[], $coupon=0.0) {
-		$sum = array_sum($items);
-		if (!is_null($sum)) {
-			return $sum - ($sum * $coupon);
-		}
-		return $sum;
-	}
+    public function total(array $items=[], $coupon=0.0) {
+        $sum = array_sum($items);
+        if (!is_null($sum)) {
+            return $sum - ($sum * $coupon);
+        }
+        return $sum;
+    }
 
-	/**
-	 * @param double $amount the amount of item
-	 * @param double $tax    the taxs
-	 * @return double
-	 */
-	public function tax($amount, $tax)
-	{ 
-		return $amount * $tax;
-	}
+    public function tax($amount, $tax) { 
+        return $amount * $tax;
+    }
 
-	/**
-	 * @param array $items  osef
-	 * @param float $tax    osef
-	 * @param float $coupon osef
-	 * @return float
-	 */
-	public function postTaxTotal($items, $tax, $coupon)
-	{
-		$subtotal = $this->total($items, $coupon);
-		return $subtotal + $this->tax($subtotal, $tax);
-	}
+    public function postTaxTotal($items, $tax, $coupon) {
+        $subtotal = $this->total($items, $coupon);
+        return $subtotal + $this->tax($subtotal, $tax);
+    }
 }
