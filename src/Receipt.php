bc730215ab9069415150715a14137578b7f903da
Gamliel Cohen <gcohen@adventureconseil.com>
2018-06-25 10:28:57 +0200

    style(src): Changed the indentation and stuff in order to test phpcs

--- src/Receipt.php
+++ src/Receipt.php
@@ -7,20 +7,39 @@ namespace TDD;
  */
 class Receipt
 {
-    public function total(array $items=[], $coupon=0.0) {
-        $sum = array_sum($items);
-        if (!is_null($sum)) {
-            return $sum - ($sum * $coupon);
-        }
-        return $sum;
-    }
+	/**
+	 * @param array $items  The items
+	 * @param float $coupon The coupon 
+	 * @return float
+	 */
+	public function total(array $items=[], $coupon=0.0)
+	{
+		$sum = array_sum($items);
+		if (!is_null($sum)) {
+			return $sum - ($sum * $coupon);
+		}
+		return $sum;
+	}
 
-    public function tax($amount, $tax) {
-        return $amount * $tax;
-    }
+	/**
+	 * @param float $amount The amount
+	 * @param float $tax    The tax
+	 * @return float
+	 */
+	public function tax($amount, $tax)
+	{
+		return $amount * $tax;
+	}
 
-    public function postTaxTotal($items, $tax, $coupon) {
-        $subtotal = $this->total($items, $coupon);
-        return $subtotal + $this->tax($subtotal, $tax);
-    }
+	/**
+	 * @param array $items  The items
+	 * @param float $tax    The tax
+	 * @param float $coupon The coupon
+	 * @return float
+	 */
+	public function postTaxTotal($items, $tax, $coupon)
+	{
+		$subtotal = $this->total($items, $coupon);
+		return $subtotal + $this->tax($subtotal, $tax);
+	}
 }
